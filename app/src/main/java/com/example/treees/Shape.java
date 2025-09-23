165d41949fde05cb18c9edbb98e3d86f582c08fb
Jugtoj Sidhu <jugtojsidhu@gmail.com>
2025-09-23 17:38:18 -0600

    added class Shape (java file)

---
+++ app/src/main/java/com/example/treees/Shape.java
@@ -0,0 +1,10 @@
+package com.example.treees;
+
+public abstract class Shape {
+    int x, y;
+
+    public abstract void getArea(int x, int y);
+    public abstract void getPerimeter(int x, int y);
+    public abstract void getShape();
+
+}
