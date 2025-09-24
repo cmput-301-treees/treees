8c6c78b2f42ae592a13c1359167c202f30082e57
Ishaan Ratanshi <i.ratanshi1@gmail.com>
2025-09-23 18:21:11 -0600

    suqare

--- app/src/main/java/com/example/treees/Square.java
+++ app/src/main/java/com/example/treees/Square.java
@@ -2,10 +2,17 @@ package com.example.treees;
 
 public class Square extends Shape {
     @Override
-    public void getArea(int x, int y);
-    @Override
-    public void getPerimeter(int x, int y);
+    public void getArea(int x, int y) {
+        System.out.println("Area of Square: " + (x * y));
+    }
 
     @Override
-    public void getShape();
+    public void getPerimeter(int x, int y) {
+        System.out.println("Perimeter of Square: " + (4 * x));
+    }
+
+    @Override
+    public void getShape() {
+        System.out.println("This is a Square");
+    }
 }
