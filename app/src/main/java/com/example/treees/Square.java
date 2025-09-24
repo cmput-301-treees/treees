e5408405dadfbe5c0cc56762150596a0dadb8edd
Ishaan Ratanshi <i.ratanshi1@gmail.com>
2025-09-23 18:12:07 -0600

    square

---
+++ app/src/main/java/com/example/treees/Square.java
@@ -0,0 +1,11 @@
+package com.example.treees;
+
+public class Square extends Shape {
+    @Override
+    public void getArea(int x, int y);
+    @Override
+    public void getPerimeter(int x, int y);
+
+    @Override
+    public void getShape();
+}
