8a40f555467b1ca864c0e2a4633e67732842986a
jonpu27 <159580290+jonpu27@users.noreply.github.com>
2025-09-23 18:28:13 -0600

    Added Triangle

--- app/src/main/java/com/example/treees/Triangle.java
+++ app/src/main/java/com/example/treees/Triangle.java
@@ -1,4 +1,19 @@
 package com.example.treees;
 
-public class Triangle {
+public class Triangle extends Shape{
+
+    @Override
+    public void getArea(int x, int y) {
+        System.out.println("The area is " + ((x * y) / 2));
+    }
+
+    @Override
+    public void getPerimeter(int x, int y) {
+
+    }
+
+    @Override
+    public void getShape() {
+
+    }
 }
