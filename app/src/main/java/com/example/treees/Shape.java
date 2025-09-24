353a9f0eb52a59619870b4d3387dfcb03ee30898
jpatel2218 <jaimepatel123@gmail.com>
2025-09-23 18:36:58 -0600

    added colour

--- app/src/main/java/com/example/treees/Shape.java
+++ app/src/main/java/com/example/treees/Shape.java
@@ -2,6 +2,7 @@ package com.example.treees;
 
 public abstract class Shape {
     int x, y;
+    String colour = "$PUT_YOUR_CHOICE_OF_COLOUR";
 
     public abstract void getArea(int x, int y);
     public abstract void getPerimeter(int x, int y);
